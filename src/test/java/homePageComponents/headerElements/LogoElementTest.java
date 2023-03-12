6f9ec4be27e3d763abca4565fdfb56c918dab074
marah2001 <71592330+marah2001@users.noreply.github.com>
2023-03-12 09:59:33 +0200

    commit this

--- src/test/java/homePageComponents/headerElements/LogoElementTest.java
+++ src/test/java/homePageComponents/headerElements/LogoElementTest.java
@@ -16,6 +16,14 @@ public class LogoElementTest {
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.get("https://manzilik.com");
+        driver.findElement(By.xpath("//*[@id='root']/div[2]/div[1]/header/div/div/div/div/div[1]/div[1]/a/img"))
+                .click();
+      String logoTagName =   driver.findElement(By.xpath("//*[@id='root']/div[2]/div[1]/header/div/div/div/div/div[1]/div[1]/a/img"))
+                .getTagName();
+        System.out.println("logo tag name is : " + logoTagName);
+         String title = driver.getTitle().trim();
+        System.out.println(title );
+
 
     }
 }
